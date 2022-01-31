7d38441197b9a335228aa9c0e38b94029269a227
RichieVan <richievan2@mail.ru>
2022-01-31 21:16:54 +0500

    Перенос лайаутов

--- webpack.config.js
+++ webpack.config.js
@@ -76,6 +76,7 @@ module.exports = {
     new HTMLWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/index.pug'),
+      favicon: './src/assets/favicon.ico',
       minify: false,
     }),
     // new HTMLWebpackPlugin({
@@ -93,31 +94,37 @@ module.exports = {
     new HTMLWebpackPlugin({
       filename: 'ui-kit/forms/index.html',
       template: path.resolve(__dirname, 'src/pages/ui-kit/forms.pug'),
+      favicon: './src/assets/favicon.ico',
       minify: false,
     }),
     new HTMLWebpackPlugin({
       filename: 'ui-kit/heading-n-footer/index.html',
       template: path.resolve(__dirname, 'src/pages/ui-kit/heading-n-footer.pug'),
+      favicon: './src/assets/favicon.ico',
       minify: false,
     }),
     new HTMLWebpackPlugin({
       filename: 'login/index.html',
       template: path.resolve(__dirname, 'src/pages/login.pug'),
+      favicon: './src/assets/favicon.ico',
       minify: false,
     }),
     new HTMLWebpackPlugin({
       filename: 'registration/index.html',
       template: path.resolve(__dirname, 'src/pages/registration.pug'),
+      favicon: './src/assets/favicon.ico',
       minify: false,
     }),
     new HTMLWebpackPlugin({
       filename: 'catalog/index.html',
       template: path.resolve(__dirname, 'src/pages/catalog.pug'),
+      favicon: './src/assets/favicon.ico',
       minify: false,
     }),
     new HTMLWebpackPlugin({
       filename: 'room/index.html',
       template: path.resolve(__dirname, 'src/pages/room.pug'),
+      favicon: './src/assets/favicon.ico',
       minify: false,
     }),
     new CopyWebpackPlugin({
